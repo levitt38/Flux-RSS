ba9fe48b4404273ae824bed6ffb1073657df8356
MOUHAT COURVOISIER Louis <mouhatcl@hostname.iut2.upmf-grenoble.fr>
2016-10-10 14:37:59 +0200

    yep

--- model/RSS.class.php
+++ model/RSS.class.php
@@ -1,4 +1,5 @@
 <?php
+require_once("../model/Nouvelle.class.php");
 class RSS {
         private $titre; // Titre du flux
         private $url;   // Chemin URL pour télécharger un nouvel état du flux
@@ -29,19 +30,36 @@ class RSS {
 
 	// Récupère un flux à partir de son URL
 	function update() {
-        // Cree un objet pour accueillir le contenu du RSS : un document XML
-          $doc = new DOMDocument;
+	        // Cree un objet pour accueillir le contenu du RSS : un document XML
+        	$doc = new DOMDocument;
 
-        //Telecharge le fichier XML dans $rss
-          $doc->load($this->url);
+	        //Telecharge le fichier XML dans $rss
+	        $doc->load($this->url);
 
-        // Recupère la liste (DOMNodeList) de tous les elements de l'arbre 'title'
-          $nodeList = $doc->getElementsByTagName('title');
+	        // Recupère la liste (DOMNodeList) de tous les elements de l'arbre 'title'
+	        $nodeList = $doc->getElementsByTagName('title');
 
-        // Met à jour le titre dans l'objet
-          $this->titre = $nodeList->item(0)->textContent;
-      }
+	        // Met à jour le titre dans l'objet
+		$this->titre = $nodeList->item(0)->textContent;
+
+		$nomLocalImage=1;
+		// Recupère tous les items du flux RSS
+
+		$this->nouvelles = [];
+
+		foreach ($doc->getElementsByTagName('item') as $node) {
+			$nouvelle = new Nouvelle();
+
+			// Met à jour la nouvelle avec l'information téléchargée
+			$nouvelle->update($node);
+
+			// Téléchage l'image
+			$nouvelle->downloadImage($node,$nomLocalImage);
+			$nomLocalImage ++;
+			$this->nouvelles[] = $nouvelle;
+		}
 
 
+	}
 }
 ?>
--- model/RSS.test.php
+++ model/RSS.test.php
@@ -1,6 +1,6 @@
 <?php
 	// Test de la classe RSS
-	require_once('RSS.test.php');
+	require_once('RSS.class.php');
 
 	// Une instance de RSS
 	$rss = new RSS('http://www.lemonde.fr/m-actu/rss_full.xml');
@@ -11,10 +11,11 @@
 	// Affiche le titre
 	echo $rss->titre()."\n";
 
-
-
-
-
+      // Affiche le titre et la description de toutes les nouvelles
+      foreach($rss->nouvelles() as $nouvelle) {
+        echo ' '.$nouvelle->titre().' '.$nouvelle->date()."\n";
+        echo '  '.$nouvelle->description()."\n";
+      }
 
 
 ?>
